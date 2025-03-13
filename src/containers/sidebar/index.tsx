8ff2bccfc782cf03c9249eb1a63feba35f800783
zezaltar <josezaltar@gmail.com>
2025-03-13 15:31:53 -0300

    Estilizando a sidebar

--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, Botao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/zezaltar.png" />
-    <Titulo fontSize={20}>Jose Zaltar</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Jose Zaltar</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        zezaltar
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Front-End
+      </Descricao>
+      <Botao>Trocar tema</Botao>
+    </SidebarContainer>
   </aside>
 )
 
